2899241cef4618f2906a7b8b8d5e0d07a6d5d2dc
Vivekanand-Yadav7 <vivekanand_yadav@srmap.edu.in>
2026-06-23 16:02:45 +0530

    bug fixed

--- server/src/services/registerUser.ts
+++ server/src/services/registerUser.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import bcrypt from 'bcryptjs';
-import prisma from '../config/db.js';
+import { prisma } from '../config/db.js';
 
 const registerUser = async (req: Request, res: Response): Promise<void> => {
     try {
