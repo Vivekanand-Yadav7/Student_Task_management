efaea54127c1c301e7f1fd14eaec829682831ff1
Vivekanand-Yadav7 <vivekanand_yadav@srmap.edu.in>
2026-06-30 14:23:31 +0530

    client deployment fixed

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
 import {
   Home,
